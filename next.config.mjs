b62a2f52aa439d4745d0d48e81cb34ecc13e0055
freewindias <freewindias6@gmail.com>
2024-06-17 20:06:00 +0530

    h

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
+  distDir: "build",
   images: {
     remotePatterns: [
       {
@@ -15,9 +15,16 @@ const nextConfig = {
       },
     ],
   },
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
-
-
-
 
 export default nextConfig;
