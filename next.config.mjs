d0195c72aa22e1cfcfb6a55d6c2b8cc0946459b9
freewindias <freewindias6@gmail.com>
2024-06-20 00:22:18 +0530

    datarevalidate

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
       },
     ],
   },
-  output: "export",
+  // output: "export",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
